54904b2ce8d6f4d62886859f433d585ef0c13e07
Jorge Clavijo <jym272@gmail.com>
2023-04-24 15:43:15 -0300

    feat: fixed test (#3)
    * feat: fixed test
    
    * feat: fixed test
    
    * feat: fixed test
    
    * feat: fixed test

--- src/server.ts
+++ src/server.ts
@@ -18,10 +18,6 @@ void (async () => {
   try {
     await startJetStream({
       queueGroupName,
-      // ->All streams in the list are created with subj: stream.*
-      // ->To publish to orders.* it must exist the stream orders -> the consumer for this stream are not necceary
-      // this api doesnt subscribe to itlsef ??? but those conmusers are created nonetheless
-      // ->To subscribe to the subjects tickets.create ........ The consumers are created with a binding durable name
       streams: [Streams.ORDERS, Streams.TICKETS, Streams.EXPIRATION, Streams.PAYMENTS],
       nats: {
         url: `nats://${getEnvOrFail('NATS_SERVER_HOST')}:${getEnvOrFail('NATS_SERVER_PORT')}`
@@ -29,7 +25,6 @@ void (async () => {
     });
     await startSetup(server);
     server.listen(PORT, () => successConnectionMsg(`${rocketEmoji} Server is running on port ${PORT}`));
-    // TODO: logs red and green and yellow with chalk
     void subscribe(subjects.TicketCreated, queueGroupName, createTicketListener);
     void subscribe(subjects.TicketUpdated, queueGroupName, updateTicketListener);
     void subscribe(subjects.ExpirationComplete, queueGroupName, expirationCompleteListener);
